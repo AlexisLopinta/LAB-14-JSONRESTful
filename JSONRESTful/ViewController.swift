32400a648d189a2a55d08fc80b577a1e4149ce15
Alexis Lopinta <alexis.lopinta@tecsup.edu.pe>
2024-06-10 14:03:57 -0500

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Alexis Frank Lopinta Ala on 10/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
